dedf921a84a5ec68f395fd3fc0bd21d24d95fad2
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-05-19 02:51:09 -0300

    Plotation strategy changed.

--- image-segmentation/seg.py
+++ image-segmentation/seg.py
@@ -11,6 +11,7 @@ if len(sys.argv) == 2:
 
 # imagem BGR sem nenhum processamento
 src_img = cv2.imread(img_path)
+
 # converte a imagem para tons de cinza
 gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
 
@@ -25,15 +26,13 @@ mask = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                              cv2.THRESH_BINARY, 115, 1)
 
 # relacionado à convolução
-# TODO: STUDY THIS
-# Acho que entendi: vai fazer a convolução e se todos os pixels no kernel forem 1, fica 1 senão, fica 0 (é isso ai)
 kernel = np.ones((int(sys.argv[2]), int(sys.argv[2])), np.uint8)
 
 # realiza a erosão da imagem
-eroded_img = cv2.erode(src_img, kernel, iterations=1)
+eroded_img = cv2.erode(mask, kernel, iterations=1)
 
 # realiza a dilatação da imagem
-dilated_img = cv2.dilate(src_img, kernel, iterations=1)
+dilated_img = cv2.dilate(mask, kernel, iterations=1)
 
 # erosão seguida de uma dilatação
 er_di_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -41,17 +40,17 @@ er_di_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 # dilatação seguida de uma erosão
 di_er_img = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
-res_mtrx = [[src_img, "Original"], [mask, "Binarizada"], [eroded_img, "Erosão"],
-           [dilated_img, "Dilatação"], [er_di_img, "Erosão e depois Dilatação"],
-           [di_er_img, "Dilatação e depois Erosão"]]
+plot_img([src_img, "Original"], [mask, "Binarizada"], [eroded_img, "Erosão"],
+         [dilated_img, "Dilatação"], [er_di_img, "Erosão e depois Dilatação"],
+         [di_er_img, "Dilatação e depois Erosão"])
 
-# plot_img([src_img, "Original"], [mask, "Binarizada"], [eroded_img, "Erosão"],
-#          [dilated_img, "Dilatação"], [er_di_img, "Erosão e depois Dilatação"],
-#          [di_er_img, "Dilatação e depois Erosão"])
+pyplot.show()
+
+# res_mtrx = [[src_img, "Original"], [mask, "Binarizada"], [eroded_img, "Erosão"],
+#            [dilated_img, "Dilatação"], [er_di_img, "Erosão e depois Dilatação"],
+#            [di_er_img, "Dilatação e depois Erosão"]]
 #
-# pyplot.show()
-
-for img, title in res_mtrx:
-    cv2.imshow(title, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# for img, title in res_mtrx:
+#     cv2.imshow(title, img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
